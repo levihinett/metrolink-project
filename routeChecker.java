d0ba95de9b7c6710160eefd7ec9664859c2c241f
levihinett <levihinett@gmail.com>
2026-05-04 19:54:01 +0100

    djikstra test 3 billion

--- routeChecker.java
+++ routeChecker.java
@@ -14,6 +14,8 @@ public class routeChecker
         HashMap<String, Double> distances = new HashMap<>(); // stores best time for each station
         HashMap<String, String> previous = new HashMap<>(); // where we came from
 
+        HashMap<String, String> previousLine = new HashMap<>(); // what line were we in order to get to where we are
+
         for (String station : graph.getStations())
         {
             distances.put(station, Double.MAX_VALUE); // sets every connection to basically inf
@@ -46,10 +48,17 @@ public class routeChecker
 
                 double laterDistance = nowDistance + edge.time; // how long to follow that node
 
+                String lastLine = previousLine.get(nowStation); // actually determine what line we were on
+                if (lastLine != null && !lastLine.equals(edge.line))
+                {
+                    laterDistance +=2; // if last line isn't blank, and equals a different line, add 2m to journey
+                }
+
                 if (laterDistance < distances.get(laterStation)) // is this faster than the last one
                 {
                     distances.put(laterStation, laterDistance); // if it is, add it
                     previous.put(laterStation, nowStation); // track our last location
+                    previousLine.put(laterStation, edge.line);
                     pq.add(laterStation); // we can explore later
                 }
             }
@@ -74,11 +83,21 @@ public class routeChecker
         Collections.reverse(route); 
 
         // output
-        System.out.println("Shortest route:");
-        for (String station : route)
+        String lastLine = null; // no line yet to be used
+        for (int i = 0; i < route.size() - 1; i++) // minus 1 as we're always looking ahead 1
         {
-            System.out.println(station);
+            String from = route.get(i); // nowStation
+            String to = route.get(i + 1); // laterStation
+            String line = previousLine.get.to(); // line used on the next stop
+
+            // check if the line has changed
+            if (lastLine != null && !lastLine.equals(line))
+            { // making sure we haven't already travelled, if different needs to swap lines
+                System.out.println("Change at " + from + " (takes 2m to swap lines)");
+            }
+            System.out.println(from + " ->" + to + " on " + line + " line");
+            lastLine = line; // remember to see if it changes
         }
-        System.out.println("Total time: " + distances.get(end) + "mins");
+        System.out.println("Total time: " + distances.get(end) + " mins");
     }
 }
